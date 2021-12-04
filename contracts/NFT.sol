87dff13b4f14c1004a2c2e5cf8d0cd0c6b85bd95
ffakira <55924290+ffakira@users.noreply.github.com>
2021-12-04 17:40:00 +0800

    fixed: minor fix

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 import "./Presale.sol";
